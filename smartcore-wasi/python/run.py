0ff54007770e19a79813f3e3c45520304aa798b9
proohit <direnc99@gmail.com>
2021-05-13 11:15:11 +0200

    smartcore-wasi - python example cleanup

--- smartcore-wasi/python/run.py
+++ smartcore-wasi/python/run.py
@@ -1,7 +1,4 @@
-import os
-import sys
 import wasmtime
-from importlib import resources as importlib_resources # py3.7+ stdlib
 
 wasm_cfg = wasmtime.Config()
 wasm_cfg.cache = True
@@ -17,10 +14,7 @@ store = wasmtime.Store(wasmtime.Engine(wasm_cfg))
 linker = wasmtime.Linker(store)
 wasi = linker.define_wasi(wasmtime.WasiInstance(store,
     "wasi_snapshot_preview1", wasi_cfg))
-yosys = linker.instantiate(wasmtime.Module(store.engine,
+smartcore_wasi = linker.instantiate(wasmtime.Module(store.engine,
     open('smartcore_wasi_lib.wasm', 'rb').read()))
 
-try:
-    yosys.exports["load_model"]()
-except wasmtime.ExitTrap as trap:
-    sys.exit(trap.code)
+smartcore_wasi.exports["load_model"]()
