9a423712d62b30a96f3dd68642293336fa0d08d4
proohit <direnc99@gmail.com>
2021-05-19 15:39:07 +0200

    smartcore-ssvm - add azure fn

---
+++ smartcore-ssvm/azure-functions/update-trigger/index.js
@@ -0,0 +1,57 @@
+module.exports = async function (context, req) {
+    const { release, repository } = req.body;
+    const { tag_name } = release;
+    const { html_url } = repository;
+
+    const Client = require('azure-iothub').Client;
+    const Message = require('azure-iot-common').Message;
+    const Registry = require('azure-iothub').Registry;
+    const { readFileSync } = require('fs');
+    const config = JSON.parse(readFileSync(`${__dirname}/config.json`));
+    const hubConnectionString = config.hubConnection;
+    const hubDevicesConnectionString = config.registryReadConnection;
+    let targetDevices = [];
+    const onDevicesQuery = (err, results) => {
+        if (err) {
+            console.error('Failed to fetch the results: ' + err.message);
+        } else {
+            targetDevices = results.map(twin => twin.deviceId);
+            console.log(targetDevices);
+        }
+    };
+    Registry.fromConnectionString(hubDevicesConnectionString).createQuery('SELECT * FROM devices').nextAsTwin(onDevicesQuery);
+    let serviceClient = Client.fromConnectionString(hubConnectionString);
+
+    function printResultFor(op) {
+        return function printResult(err, res) {
+            if (err) console.log(op + ' error: ' + err.toString());
+            if (res) console.log(op + ' status: ' + res.constructor.name);
+        };
+    }
+    function receiveFeedback(err, receiver) {
+        receiver.on('message', function (msg) {
+            console.log('Feedback message:');
+            console.log(msg.getData().toString('utf-8'));
+        });
+    }
+    serviceClient.open(function (err) {
+        if (err) {
+            console.error('Could not connect: ' + err.message);
+        } else {
+            console.log('Service client connected');
+            serviceClient.getFeedbackReceiver(receiveFeedback);
+            var message = new Message(JSON.stringify({
+                action: 'update',
+                url: `${html_url}/releases/download`,
+                version: tag_name,
+                runtimes: ['js']
+            }));
+            message.ack = 'full';
+            message.messageId = 'My Message ID';
+            console.log('Sending message: ' + message.getData());
+            for (const targetDevice of targetDevices) {
+                serviceClient.send(targetDevice, message, printResultFor('send'));
+            }
+        }
+    });
+}
