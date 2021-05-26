9e15e642b4468004dbeeda9091621f3bd62ef393
proohit <direnc99@gmail.com>
2021-05-26 15:07:02 +0200

    smartcore-wasi - added arg to specify output file

--- smartcore-wasi/src/main.rs
+++ smartcore-wasi/src/main.rs
@@ -1,12 +1,16 @@
 use chrono::Utc;
 use smartcore_wasi_lib::{init, load_model};
+use std::env::args;
 use std::time::Instant;
 
 fn main() {
     let mut path = String::from("iris_knn.model\0");
     let ptr = path.as_mut_ptr();
     std::mem::forget(ptr);
+    #[cfg(target_arch = "x86_64")]
     init(ptr.cast::<i8>());
+    #[cfg(all(target_arch = "aarch64", target_arch = "arm"))]
+    init(ptr);
     let mut performances: Vec<u128> = Vec::new();
     let start_time = Utc::now();
     for _ in 0..1000 {
@@ -21,10 +25,16 @@ fn main() {
     let os = std::env::consts::OS;
     let os_type = info.os_type();
     let os_version = info.version();
-
+    let args: Vec<String> = args().collect();
+    let mut output_file = String::from("data.csv");
+    for arg in args {
+        if arg.contains(".csv") {
+            output_file = arg;
+        }
+    }
     let mut wtr = csv::WriterBuilder::new()
         .has_headers(true)
-        .from_path("data.csv")
+        .from_path(&output_file)
         .unwrap();
     wtr.write_record(&[
         "startTime",
@@ -49,4 +59,5 @@ fn main() {
         .unwrap();
     }
     wtr.flush().unwrap();
+    println!("Written benchmark data to {}", output_file);
 }
